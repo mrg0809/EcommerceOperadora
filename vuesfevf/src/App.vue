03a771a5f116145eb4b8830f7d7efc6a152d5563
Marcelino Rodriguez <mrg0809@gmail.com>
2021-06-23 23:53:09 -0500

    creando django y vue para tienda

---
+++ vuesfevf/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>EVF</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-end">
+          <router-link to="/dama" class="navbar-item">Dama</router-link>
+          <router-link to="/caballero" class="navbar-item">Caballero</router-link>
+          <router-link to="/ninios" class="navbar-item">Youth</router-link>
+          <router-link to="/marcas" class="navbar-item">Marcas</router-link>
+          <router-link to="/ubicaciones" class="navbar-item">Ubicaciones</router-link>
+          
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Carrito</span>
+              </router-link>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">EVF 2021</p>
+    </footer>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../node_modules/bulma';
+</style>
---
+++ vuesfevf/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    EVF
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
